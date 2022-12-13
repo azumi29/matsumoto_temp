e392d16ba234470cc75da2f965a1f4349e9ff5ad
azumi <adumi29deviser@gmail.com>
2022-12-13 10:23:38 +0900

    utf-8

---
+++ matsumoto_temp_utf8.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import japanize_matplotlib
+ 
+#CSVファイルをANSI形式で読み込む
+data = pd.read_csv('matsumoto_temp_utf8.csv')
+print(data)
+
+# グラフに出力
+data.plot()
+plt.title('松本市の気温')
+plt.xlabel('日付')
+plt.ylabel('気温')
+
+# グラフをpngに保存
+plt.savefig("matsumoto_temp_utf801.png")
+
+plt.show()
